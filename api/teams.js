563c8308d59dda5e36c31c6270b3e5ec05ba261b
Hannah3141 <hannahmalini@gmail.com>
2026-01-26 12:06:54 -0600

    define wiTeams

--- api/teams.js
+++ api/teams.js
@@ -38,6 +38,8 @@ export default async function handler(req, res) {
     let totalPages = 1;
     let results = [];
 
+    let wiTeams = [];
+
     while (page <= totalPages) {
       const url = `https://ftc-api.firstinspires.org/v2.0/${season}/teams?page=${page}`;
     
